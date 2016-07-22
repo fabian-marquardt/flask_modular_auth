06469e27e237a6c551e72e67f5345fcc6594e89a
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-07-22 14:12:33 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+from setuptools import setup, find_packages
+
+setup(
+    name="flask_modular_auth",
+    version=subprocess.check_output(['git', 'describe', '--always'], cwd=os.path.dirname(os.path.abspath(__file__))).strip().decode('utf-8'),
+    packages=find_packages(),
+    install_requires=[]
+)
