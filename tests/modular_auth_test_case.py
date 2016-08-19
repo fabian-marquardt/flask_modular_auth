becd4a6aa6be310c9ae3be75d0172dcf464ee391
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-08-19 13:41:56 +0200

    Fixing failing super calls for Python 2.7 ... again

--- tests/modular_auth_test_case.py
+++ tests/modular_auth_test_case.py
@@ -5,6 +5,6 @@ from flask_modular_auth import AuthManager
 
 class ModularAuthTestCase(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(ModularAuthTestCase, self).__init__(*args, **kwargs)
         self.app = Flask(__name__)
         AuthManager(self.app)
